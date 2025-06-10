6826e53ad9dff12e2ac81a6ac46f028f1d4bfd10
Łukasz Tołwiński <lukasz.adam.tolwinski@gmail.com>
2025-06-10 14:27:28 +0200

    Add Tailwind styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,19 @@ import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-lg">Home Page</h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
 
-      <Link href={ticketsPath()} className="underline">
-        Go to Tickets
-      </Link>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline">
+          Go to Tickets
+        </Link>
+      </div>
     </div>
   );
 };
--- src/data.ts
+++ src/data.ts
@@ -2,13 +2,13 @@ export const initialTickets = [
   {
     id: "1",
     title: "Ticket 1",
-    content: "This is the content of ticket 1.",
+    content: "This is the first content of ticket 1.",
     status: "DONE" as const,
   },
   {
     id: "2",
     title: "Ticket 2",
-    content: "This is the content of ticket 2.",
+    content: "This is the second content of ticket 2.",
     status: "OPEN" as const,
   },
   {
