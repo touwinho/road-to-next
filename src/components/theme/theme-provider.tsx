af18e67c373f4da6ac72b243455945ca17e2f905
Łukasz Tołwiński <lukasz.adam.tolwinski@gmail.com>
2025-06-10 18:10:54 +0200

    Implement theme switch

---
+++ src/components/theme/theme-provider.tsx
@@ -0,0 +1,14 @@
+import {
+  ThemeProvider as BaseThemeProvider,
+  ThemeProviderProps,
+} from "next-themes";
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  return (
+    <BaseThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </BaseThemeProvider>
+  );
+};
+
+export { ThemeProvider };
--- src/features/ticket/constans.tsx
+++ src/features/ticket/constans.tsx
@@ -1,7 +1,11 @@
-import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
+import {
+  LucideCircleCheckBig,
+  LucideFileText,
+  LucidePencil,
+} from "lucide-react";
 
 export const TICKET_ICONS = {
   OPEN: <LucideFileText />,
   IN_PROGRESS: <LucidePencil />,
-  DONE: <LucideCircleCheck />,
+  DONE: <LucideCircleCheckBig />,
 };
