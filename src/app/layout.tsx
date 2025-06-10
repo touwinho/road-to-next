e84802211f3a25ac224324205fb3eddecaf1aa4b
Łukasz Tołwiński <lukasz.adam.tolwinski@gmail.com>
2025-06-10 15:45:18 +0200

    Implement UI Library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import { homePath, ticketsPath } from "@/paths";
+import { buttonVariants } from "@/components/ui/button";
+import { LucideKanban } from "lucide-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,12 +33,19 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div>
-            <Link href={homePath()} className="text-lg font-bold">
-              Home
+            <Link
+              href={homePath()}
+              className={buttonVariants({ variant: "ghost" })}
+            >
+              <LucideKanban />
+              <h1 className="text-lg font-semibold">TicketBounty</h1>
             </Link>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-sm underline">
+            <Link
+              href={ticketsPath()}
+              className={buttonVariants({ variant: "default" })}
+            >
               Tickets
             </Link>
           </div>
